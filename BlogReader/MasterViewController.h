6feb674d1d3fce3a659f53fa4f47aabfda6419da
Brandon Smith <brandon.smith@yale.edu>
2013-03-14 16:41:12 -0400

    Initial Commit

---
+++ BlogReader/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  BlogReader
+//
+//  Created by Brandon Smith on 3/14/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
